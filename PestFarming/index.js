79e78c7b2f4b392164a3882b9960b20db6533cbd
ReaperPV <joshua.ware123@gmail.com>
2024-11-23 22:38:16 +0000

    feat: set lower cooldown if finnegan has pest farming perks

--- PestFarming/index.js
+++ PestFarming/index.js
@@ -1,3 +1,5 @@
+import request from "../requestV2";
+
 const File = Java.type("java.io.File");
 const AudioSystem = Java.type("javax.sound.sampled.AudioSystem");
 
@@ -13,13 +15,25 @@ function playSystemSound(filePath) {
     }
 }
 
+function isFinnegan() {
+    return request({
+        url: 'https://api.hypixel.net/v2/resources/skyblock/election',
+        json: true
+    })
+    .then((response) => {
+        return response.mayor.perks.some(perk => perk.name === "Pest Eradicator") || response.minister.perk.name === "Pest Eradicator";
+    })
+    .catch((err) => console.error(`PestFarming: ${err.cause ?? err}`));
+}
+
+const cooldown = isFinnegan() ? 55000 : 115000
+
 register("chat", (player, event) => {
     setTimeout(() => {
         ChatLib.chat("&2&lPest Farming: &c&lSwap Armour Now!");
 
         Client.showTitle("&c&lSwap Armour Now!", "You stupid idiot", 0.5, 50, 0.5);
 
-        playSystemSound("/assets/anvil-drop.wav");
-
-    }, 55000);
+        playSystemSound("./config/ChatTriggers/modules/PestFarming/assets/anvil-drop.wav");
+    }, cooldown);
 }).setCriteria("&r&6&lYUCK!").setContains();
