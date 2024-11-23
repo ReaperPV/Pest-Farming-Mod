fc6a084b9f8d6099e2190a4adc12b649f9e5dfd5
ReaperPV <joshua.ware123@gmail.com>
2024-11-23 17:14:44 +0000

    initial commit

---
+++ PestFarming/index.js
@@ -0,0 +1,25 @@
+const File = Java.type("java.io.File");
+const AudioSystem = Java.type("javax.sound.sampled.AudioSystem");
+
+function playSystemSound(filePath) {
+    try {
+        let soundFile = new File(filePath);
+        let audioStream = AudioSystem.getAudioInputStream(soundFile);
+        let clip = AudioSystem.getClip();
+        clip.open(audioStream);
+        clip.start();
+    } catch (error) {
+        ChatLib.chat("&cError playing sound: " + error);
+    }
+}
+
+register("chat", (player, event) => {
+    setTimeout(() => {
+        ChatLib.chat("&2&lPest Farming: &c&lSwap Armour Now!");
+
+        Client.showTitle("&c&lSwap Armour Now!", "You stupid idiot", 0.5, 50, 0.5);
+
+        playSystemSound("./config/ChatTriggers/modules/PestFarming/assets/anvil-drop.wav");
+
+    }, 55000);
+}).setCriteria("&r&6&lYUCK!").setContains();
